e4203f448ce418091aeead7b5b441dae909e30fd
Henrik Fredriksson <henrikfredriksson2@gmail.com>
2019-10-03 14:57:54 +0200

    updating index route

--- routes/index.js
+++ routes/index.js
@@ -10,7 +10,8 @@ router.get('/', function (req, res, next) {
 
     <p>
       Detta är min me- sida i kursen jsramverk på Blekinge Tekniska Högskola.
-    </p> <p>
+    </p>
+    <p>
       Jag heter Henrik Fredriksson och är uppvuxen i Getinge
       som ligger mellan Halmstad och Falkenberg på västkusten.
       Efter att ha jobbat 2 år i en elektronikproduktion efter
@@ -22,7 +23,8 @@ router.get('/', function (req, res, next) {
       matematik för blivande civil - och högskoleingejörer.
       Jag forskar även delvis inom tillämpad matematik.Forskningen handlar om att finna
       matematiska modeller för att strategiskt placera ut mätstationer för att effektivt mäta trafikflöden.
-    </p> <p>
+    </p>
+    <p>
       Nu har dock intresset att börja studera börjat vakna igen
       och har därför börjar studera webbprogrammering på BTH.
       Programmeringintresset har de senaste åren gått lite i vågor,
@@ -30,7 +32,8 @@ router.get('/', function (req, res, next) {
       Har de senaste åren mest jobbat med matematiska programvaror
       som Matlab, Mathematica och Sage, men
       har sedan tidigare erfarenhet av bland annat C++ och Java.
-    </p> <p>
+    </p>
+    <p>
       När jag inte jobbar eller studerar så ägnas övrig tid åt idrott.Jag är bland annat ledare för
       ett ungdomslag i Karlskrona Handboll.Helgerna spenderas därför oftast i en inomhushall
       någonstans i Blekinge eller Småland.Helst en med kaffekopp i ena handen.
