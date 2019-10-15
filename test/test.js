1da4bb291c2685f795273583f1f6f0c9b40da6ce
Henrik Fredriksson <henrikfredriksson2@gmail.com>
2019-10-15 13:57:03 +0200

    fuck you Jest

---
+++ test/test.js
@@ -0,0 +1,47 @@
+process.env.NODE_ENV = 'test'
+
+const chai = require('chai')
+const chaiHttp = require('chai-http')
+const app = require('../server.js')
+
+chai.should()
+
+chai.use(chaiHttp)
+
+describe('Reports', () => {
+  describe('GET /reports/kmom01', () => {
+    it('200 HAPPY PATH', (done) => {
+      chai.request(app)
+        .get('/')
+        .end((err, response) => {
+          if (err) {
+            throw err
+          }
+          response.statusCode.should.be.equal(200)
+
+          response.body.name.should.be.equal('Henrik Fredriksson')
+          response.body.email.should.be.equal('hefa14@student.bth.se')
+          // res.should.have.status(200)
+          response.body.should.be.an('object')
+          // res.body.data.should.be.an('array')
+          // res.body.data.length.should.be.above(0)
+          done()
+        })
+    })
+  })
+
+  // describe('GET /reports/kmom02', () => {
+  //   it('200 HAPPY PATH', (done) => {
+  //     chai.request(server)
+  //       .get('/reports/kmom02')
+  //       .end((err, res) => {
+  //         res.should.have.status(200)
+  //         res.body.should.be.an('object')
+  //         res.body.data.should.be.an('array')
+  //         res.body.data.length.should.be.above(0)
+
+  //         done()
+  //       })
+  //   })
+  // })
+})
