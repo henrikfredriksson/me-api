06b1037e36ea6879eb21f142f22284d05b96e673
Henrik Fredriksson <henrikfredriksson2@gmail.com>
2019-10-19 23:41:19 +0200

    more changes

--- test/test.js
+++ test/test.js
@@ -23,34 +23,4 @@ describe('Routes', () => {
         })
     })
   })
-
-  describe('GET /users', () => {
-    it('200 HAPPY PATH', (done) => {
-      chai.request(app)
-        .get('/users')
-        .end((err, response) => {
-          if (err) {
-            throw err
-          }
-          response.statusCode.should.be.equal(200)
-          response.body.should.be.an('array')
-          done()
-        })
-    })
-  })
-
-  // describe('GET /reports/kmom02', () => {
-  //   it('200 HAPPY PATH', (done) => {
-  //     chai.request(server)
-  //       .get('/reports/kmom02')
-  //       .end((err, res) => {
-  //         res.should.have.status(200)
-  //         res.body.should.be.an('object')
-  //         res.body.data.should.be.an('array')
-  //         res.body.data.length.should.be.above(0)
-
-  //         done()
-  //       })
-  //   })
-  // })
 })
